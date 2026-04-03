91d45505a140af02524ac0886a1ea2a2a5e165f7
jodo0627 <joergdomey@gmail.com>
2026-04-03 17:36:16 +0200

    feat: update Docker and Nginx configurations for production setup

--- web-platform/next.config.ts
+++ web-platform/next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  // Ensure the dev server trusts the proxy headers
-  devIndicators: {
-    appIsrStatus: false,
-  },
+    /* config options here */
+    serverExternalPackages: ['ssh2'],
 };
 
 export default nextConfig;
